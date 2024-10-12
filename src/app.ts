b7b969c83e767c7afb3e4b516108fdbdb0e09d3a
Marlon Villalona <iamvillalona@gmail.com>
2024-10-11 20:09:10 -0400

    added the architecture of the project

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
 import dotenv from 'dotenv';
+dotenv.config()
+import express from 'express';
+
+const app = express()
+
+app.use(express.json())
+
+//Routes
+
+// Auth
+
+// User
+
+console.log('Success!');
+export default app
