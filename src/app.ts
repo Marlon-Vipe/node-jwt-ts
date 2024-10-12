d5338be182a9f7f116eb601c1199f0c0f1fab0d4
Marlon Villalona <iamvillalona@gmail.com>
2024-10-11 23:35:36 -0400

    added: jwt-prisma-docker

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config()
 import express from 'express';
+import authRoutes from './routes/auth.Routes'
 
 const app = express()
 
 app.use(express.json())
 
 //Routes
-
+app.use('/auth', authRoutes)
 // Auth
 
 // User
