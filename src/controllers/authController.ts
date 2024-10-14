7dacbc6418b4400de7bba7a045f6b5985bcacb27
Marlon Villalona <iamvillalona@gmail.com>
2024-10-13 22:11:11 -0400

    added login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from "express";
-import { hashPassword } from "../services/password.services";
+import { comparePasswords, hashPassword } from "../services/password.services";
 import prisma from "../models/user"
-import { generateToken} from "../services/auth.services"
+import { generateToken } from "../services/auth.services"
 
 export const register = async(req: Request, res: Response): Promise<void> => {
 
     const { email, password } = req.body
 
     try {
-
+        
+        if(!email) {
+            res.status(400).json({ message: 'El email es obligatorio'})
+        } 
+        if(!password) {
+            res.status(400).json({ message: 'El password es obligatorio'})
+        } 
         const hashedPassword = await hashPassword(password)
         console.log(hashedPassword);
 
@@ -20,13 +26,14 @@ export const register = async(req: Request, res: Response): Promise<void> => {
             }
         }
     )
-
     const token = generateToken(user)
     res.status(201).json({ token })
         
-    } catch (error) {
-
+    } catch (error:any) {
         // TODO: Mejorar los errores
+        if(error?.code === 'P2002' && error?.meta?.target?.includes('email')){
+            res.status(400).json({ message: 'El email ingresado ya existe'})
+        }
         console.log(error);
         res.status(500).json({
             eror: 'Hubo un error en el registro'
@@ -34,3 +41,37 @@ export const register = async(req: Request, res: Response): Promise<void> => {
     }
 
 }
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+    
+    const { email, password } = req.body
+
+    try {
+
+        if(!email) {
+            res.status(400).json({ message: 'El email es obligatorio'})
+        } 
+        if(!password) {
+            res.status(400).json({ message: 'El password es obligatorio'})
+        } 
+        
+        const user = await prisma.findUnique({ where: { email }})
+        
+        if(!user){
+            res.status(404).json({ error: 'Usuario y/o Contraseña incorrectas'})
+            return
+        }
+
+        const passwordMatch = await comparePasswords(password, user.password);
+            if(!passwordMatch){
+                res.status(401).json({error: 'Usuario y/o Contraseña no coinciden'})
+            }
+
+            const token = generateToken(user)
+            res.status(200).json({ token })
+
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+}
+
